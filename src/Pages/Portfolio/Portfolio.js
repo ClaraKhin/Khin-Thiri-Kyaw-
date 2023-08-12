a96df1416abd45ed567473410aff3f48fcb82da6
ClaraKhin <khinthirikyaw8@gmail.com>
2023-08-13 01:53:59 +0630

    Change routes

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -5,26 +5,24 @@ import { Container } from "react-bootstrap";
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">
-      <Container className="container-fluid">
-        <div className="heading text-center">
-          <h3>My Portfolio</h3>
-        </div>
-        <div className="card_wrapper">
-          {Portfolio_data.map((value, index) => {
-            return (
-              <PortfolioCard
-                key={index}
-                image={value.image}
-                title={value.title}
-                description={value.desc}
-                link={value.link}
-              />
-            );
-          })}
-        </div>
-      </Container>
-    </section>
+    <Container className="container-fluid">
+      <div className="heading text-center">
+        <h3>My Portfolio</h3>
+      </div>
+      <div className="card_wrapper">
+        {Portfolio_data.map((value, index) => {
+          return (
+            <PortfolioCard
+              key={index}
+              image={value.image}
+              title={value.title}
+              description={value.desc}
+              link={value.link}
+            />
+          );
+        })}
+      </div>
+    </Container>
   );
 };
 
--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -6,22 +6,16 @@ import { Container } from "react-bootstrap";
 
 const Skills = () => {
   return (
-    <section id="skills">
-      <Container className="skill_container">
-        <h3 className="text-center">Skills</h3>
-        <div className="wrapper">
-          {Skills_data.map((value, id) => {
-            return (
-              <SkillsCard
-                key={id}
-                image={value.image}
-                description={value.desc}
-              />
-            );
-          })}
-        </div>
-      </Container>
-    </section>
+    <Container className="skill_container">
+      <h3 className="text-center">Skills</h3>
+      <div className="wrapper">
+        {Skills_data.map((value, id) => {
+          return (
+            <SkillsCard key={id} image={value.image} description={value.desc} />
+          );
+        })}
+      </div>
+    </Container>
   );
 };
 
