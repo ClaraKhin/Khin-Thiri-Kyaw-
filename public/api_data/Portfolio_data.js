b30fd53878f4c8d1c6478a1ea15285bd280c8bf7
ClaraKhin <khinthirikyaw8@gmail.com>
2023-12-15 15:53:34 +0630

    change data info

--- public/api_data/Portfolio_data.js
+++ public/api_data/Portfolio_data.js
@@ -1,10 +1,10 @@
 const Portfolio_data = [
   {
     id: 1,
-    image: "../images/Resume Builder.JPG",
-    title: "Resume Builder App",
-    desc: " Craft Your simple and Perfect Resume Effortlessly. Get Started Now!",
-    link: "https://resume-builder-phi-six.vercel.app/",
+    image: "../images/java-script.jpg",
+    title: "Pure JavaScript Projects Collection",
+    desc: " Welcome to our JavaScript Projects Showcase, where innovation meets functionality! Let's keep the steps forward the future of web development where JavaScript is the key to unlocking limitless possibilities!",
+    link: "https://pure-javascript-projects.netlify.app/",
   },
   {
     id: 2,
