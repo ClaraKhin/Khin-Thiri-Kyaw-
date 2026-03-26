d2d599c1d982f61814aa8b87077a7f1c1db16a78
Khin Thiri Kyaw <khinthirikyaw8@gmail.com>
2026-03-26 14:35:35 +0630

    added a new nearly finished project

--- src/Pages/Portfolio/Portfolio_data.js
+++ src/Pages/Portfolio/Portfolio_data.js
@@ -8,32 +8,40 @@ const Portfolio_data = [
   },
   {
     id: 2,
+    image: "../images/Restaurant_POS.png",
+    title: "Restaurant POS System",
+    desc: "An COMING SOON full-stack POS system with real-time dashboard, order tracking, and table management using React.js, Node.js, Express, MySQL, and Redux.",
+    link: "#",
+  },
+  {
+    id: 3,
     image: "../images/Excel.png",
     title: "Excel Spreadsheet",
     desc: "A lightweight, interactive web-based spreadsheet that supports custom formulas and basic arithmetic operations. Built using vanilla JavaScript, it enables users to input expressions and automatically evaluates them, mimicking spreadsheet functionality.",
     link: "https://spreadsh33t.netlify.app/",
   },
   {
-    id: 3,
+    id: 4,
     image: "../images/Mortage-Calculator.png",
     title: "Mortgage Repayment Calculator",
     desc: "The Mortgage Calculator App helps users estimate their monthly and total mortgage payments based on loan amount, interest rate, and term.",
     link: "https://mortgage-calculat0r.netlify.app/",
   },
   {
-    id: 4,
+    id: 5,
     image: "../images/Resume-Builder.png",
     title: "Resume Builder",
     desc: "State, CRUD practice on React app for Simple Resume Builder App. ",
     link: "https://resume-builder-phi-six.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     image: "../images/Pokemon.png",
     title: "Pokémon Search App",
     desc: "API practice on a simple application that allows users to search for Pokémon by name or ID. It fetches data from the PokéAPI Proxy and displays key details supports searching with both a button click and the Enter key. ",
     link: "https://pokemon-search-buddy.netlify.app/",
   },
+
 
 ];
 
